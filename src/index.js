8e47962a3f351f84fa770a7c2ef1fd02afcbecb3
Alejandro <alejandroparadiso02@gmail.com>
2023-10-11 22:44:30 -0400

    ya

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
-
+app.use(cors())
 const variableImportada = require('./config.js');
 
 // settings
